56a40833b3805da4d9332b4ebb8b37257449d15d
Simone Renzo <simone201@gmail.com>
2012-06-15 20:54:15 +0200

    Revert "To revert"
    This reverts commit be6c062bbf6e12682782bcb9d6d9c87d3f923d15.

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -800,10 +800,6 @@ static ssize_t mem_rw(struct file *file, char __user *buf,
 			size_t count, loff_t *ppos, int write)
 {
 	struct mm_struct *mm = file->private_data;
-<<<<<<< HEAD
-=======
-
->>>>>>> ef3bc83... Fix a serious security hazzard.
 	unsigned long addr = *ppos;
 	ssize_t copied;
 	char *page;
@@ -859,11 +855,7 @@ static ssize_t mem_read(struct file *file, char __user *buf,
 }
 
 static ssize_t mem_write(struct file *file, const char __user *buf,
-<<<<<<< HEAD
 			 size_t count, loff_t *ppos)
-=======
-		size_t count, loff_t *ppos)
->>>>>>> ef3bc83... Fix a serious security hazzard.
 {
 	return mem_rw(file, (char __user*)buf, count, ppos, 1);
 }
